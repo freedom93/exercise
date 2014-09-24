c8bf36bcb9c41f3b1310aa52dcad09f2facb5f48
freedom93 <1563607296@qq.com>
2014-09-24 20:37:55 +0800

    ImageSwitcherGallery2(带缩略图列表的图片预览模式) Signed-off-by: freedom93 <1563607296@qq.com>

---
+++ ImageSwitcherGallery2/src/com/freedom/imageswitchergallery2/MainActivity.java
@@ -0,0 +1,103 @@
+package com.freedom.imageswitchergallery2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.Gallery.LayoutParams;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
+
+public class MainActivity  extends Activity implements
+AdapterView.OnItemSelectedListener, ViewSwitcher.ViewFactory {
+   
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+        setTitle("ImageSwitchAndGallery");
+
+        mSwitcher = (ImageSwitcher) findViewById(R.id.switcher);
+        mSwitcher.setFactory(this);
+        mSwitcher.setInAnimation(AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_in));
+        mSwitcher.setOutAnimation(AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_out));
+
+        Gallery g = (Gallery) findViewById(R.id.gallery);
+        g.setAdapter(new ImageAdapter(this));
+        g.setOnItemSelectedListener(this);
+    }
+
+    public void onItemSelected(AdapterView parent, View v, int position, long id) {
+        mSwitcher.setImageResource(mImageIds[position]);
+    }
+
+    public void onNothingSelected(AdapterView parent) {
+    }
+
+    public View makeView() {
+        ImageView i = new ImageView(this);
+        i.setBackgroundColor(0xFF000000);
+        i.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        i.setLayoutParams(new ImageSwitcher.LayoutParams(LayoutParams.FILL_PARENT,
+                LayoutParams.FILL_PARENT));
+        return i;
+    }
+
+    private ImageSwitcher mSwitcher;
+
+    public class ImageAdapter extends BaseAdapter {
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView i = new ImageView(mContext);
+
+            i.setImageResource(mThumbIds[position]);
+            i.setAdjustViewBounds(true);
+            i.setLayoutParams(new Gallery.LayoutParams(
+                    LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+            i.setBackgroundResource(R.drawable.picture_frame);
+            return i;
+        }
+
+        private Context mContext;
+
+    }
+ 
+	private Integer[] mThumbIds = { 
+			R.drawable.picture1,R.drawable.picture2, R.drawable.picture3, 
+			R.drawable.picture4,R.drawable.picture5,R.drawable.picture6, 
+			R.drawable.picture7, R.drawable.picture8,R.drawable.picture9};
+
+    private Integer[] mImageIds = {
+    		R.drawable.picture1,R.drawable.picture2, R.drawable.picture3, 
+			R.drawable.picture4,R.drawable.picture5,R.drawable.picture6, 
+			R.drawable.picture7, R.drawable.picture8,R.drawable.picture9};
+
+
+ 
+}
